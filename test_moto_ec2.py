035331e17124309623564348642221c32fed4bfe
andre rozendaal <andre.rozendaal@rabobank.nl>
2021-01-26 13:36:49 +0100

    init

---
+++ test_moto_ec2.py
@@ -0,0 +1,59 @@
+import boto3
+import pytest
+from moto import mock_ec2
+
+@mock_ec2
+def create_ec2_environment():
+    ec2 = boto3.client("ec2", region_name="us-east-1")
+
+    # get latest ec2 image for testing
+    image_response = ec2.describe_images()
+    image_id = image_response['Images'][0]['ImageId']
+
+    instance_response = ec2.run_instances(InstanceType="t2.micro", 
+                                    MaxCount=1, 
+                                    MinCount=1, 
+                                    ImageId=image_id)
+
+    return ec2, instance_response["Instances"][0]["InstanceId"]
+
+def get_state(ec2, instanceid):
+
+
+        # get id of created instance                                
+    response = ec2.describe_instances(
+    InstanceIds=[ instanceid
+    ],
+    )
+
+    return response["Reservations"][0]["Instances"][0]["State"]["Name"]
+
+@mock_ec2
+def test_stop_ec2():
+   
+    (ec2, instanceid) = create_ec2_environment()
+
+    ec2.stop_instances( InstanceIds=[instanceid])
+    state = get_state(ec2, instanceid)
+
+    assert state == "stopped"
+
+@mock_ec2
+def test_terminate_ec2():
+    (ec2, instanceid) = create_ec2_environment()
+
+    ec2.terminate_instances( InstanceIds=[instanceid])
+    state = get_state(ec2, instanceid)
+
+    assert state == "terminated"
+
+
+
+
+ 
+  
+
+    
+    
+
+    
