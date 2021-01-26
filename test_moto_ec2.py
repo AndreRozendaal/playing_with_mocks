332420ffab6d714ed6432bfeb3aa4cedcb42cdd7
andre rozendaal <andre.rozendaal@rabobank.nl>
2021-01-26 17:45:23 +0100

    update

--- test_moto_ec2.py
+++ test_moto_ec2.py
@@ -3,11 +3,10 @@ import pytest
 from moto import mock_ec2
 
 
-@mock_ec2
-def create_ec2_environment():
-    ec2 = boto3.client("ec2", region_name="us-east-1")
+def get_client():
+    return boto3.client("ec2", region_name="us-east-1")
 
-    # get latest ec2 image for testing
+def create_ec2(ec2):
     image_response = ec2.describe_images()
     image_id = image_response["Images"][0]["ImageId"]
 
@@ -15,8 +14,12 @@ def create_ec2_environment():
         InstanceType="t2.micro", MaxCount=1, MinCount=1, ImageId=image_id
     )
 
-    return ec2, instance_response["Instances"][0]["InstanceId"]
+    return instance_response["Instances"][0]["InstanceId"]
 
+def create_ec2_environment():
+    ec2 = get_client()
+    instanceid = create_ec2(ec2)
+    return ec2, instanceid
 
 def get_state(ec2, instanceid):
 
@@ -27,17 +30,21 @@ def get_state(ec2, instanceid):
 
     return response["Reservations"][0]["Instances"][0]["State"]["Name"]
 
+@mock_ec2 
+def test_create_ec2():
+    (ec2, instanceid) = create_ec2_environment()
+    state = get_state(ec2, instanceid)
+
+    assert state == "running"
 
 @mock_ec2
 def test_stop_ec2():
-
     (ec2, instanceid) = create_ec2_environment()
     print(instanceid)
     ec2.stop_instances(InstanceIds=[instanceid])
     state = get_state(ec2, instanceid)
 
     assert state == "stopped"
-
 
 @mock_ec2
 def test_terminate_ec2():
